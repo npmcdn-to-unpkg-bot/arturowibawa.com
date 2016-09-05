2e537cefef2aa36a98096fea588f1e81061e48e4
Arturo Wibawa <agwibawa@gmail.com>
2016-09-04 22:29:22 -0700

    social share module

--- html/js/master.js
+++ html/js/master.js
@@ -98,6 +98,56 @@ $(document).ready(function() {
 
 } ());
 
+/* SHARE BUTTON HOVER */
+(function() {
+
+	// BUTTON HOVERS
+	var shareBtn = document.querySelector('.share-btn');
+	var shareBtnBg = document.querySelector('.share-btn-bg');
+	var shareBtnClose = document.querySelector('.share-btn--close');
+	var shareBtnCloseBg = document.querySelector('.share-btn--close-bg');
+
+	// DROPDOWN
+	var dropdown = document.querySelector('.share-dropdown');
+	var dropdownItem = document.querySelectorAll('.share-dropdown-item');
+
+	var active = false;
+
+	var toggleShare = function () {
+		if (!active) {
+			dropdown.classList.add('share-dropdown--active');
+			for (var i = 0, ii = dropdownItem.length; i < ii; i++) {
+					dropdownItem[i].classList.add('share-dropdown-item--active');
+				}
+
+			active = true;
+		}	else {
+			dropdown.classList.remove('share-dropdown--active');
+			for (var i = 0, ii = dropdownItem.length; i < ii; i++) {
+					dropdownItem[i].classList.remove('share-dropdown-item--active');
+				}
+
+			active = false;
+		}
+	};
+
+	shareBtn.addEventListener('click', toggleShare, false);
+	shareBtnClose.addEventListener('click', toggleShare, false);
+
+	shareBtn.addEventListener('mouseover', mouseOver);
+	shareBtn.addEventListener('mouseout', mouseOut);
+
+	function mouseOver() {
+		shareBtnBg.classList.add('share-btn-bg--active');
+	}
+	function mouseOut() {
+		shareBtnBg.classList.remove('share-btn-bg--active');
+	}
+
+
+
+}());
+
 /* HIDE HERO HACK */
 (function() {
 
